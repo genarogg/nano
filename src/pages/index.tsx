75054ba8d6fe40e3114180ca4db74432485265a9
Genaro Gonzalez <genarrogg@gmail.com>
2024-09-07 22:59:26 -0400

    alias

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
+import { Icono } from "@nano";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -14,6 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Icono ></Icono>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>

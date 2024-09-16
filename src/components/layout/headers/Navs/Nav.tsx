e39c279d49178e994a27176a17498147d766c104
Genaro Gonzalez <genarrogg@gmail.com>
2024-09-16 12:47:30 -0400

    alyout basico

--- src/components/layout/headers/Navs/Nav.tsx
+++ src/components/layout/headers/Navs/Nav.tsx
@@ -1,7 +1,5 @@
 import { A, Icono } from "@nano";
 
-import TouchNav from "../HeaderFn/TouchWrapper";
-
 interface NavProps {
   className?: string;
   isActive?: boolean;
@@ -20,29 +18,27 @@ const Nav: React.FC<NavProps> = ({
   setIsActive,
 }) => {
   return (
-    <TouchNav>
-      <div
-        className={`container-nav ${className}  ${isActive ? "active" : ""}`}
-      >
-        <nav>
-          <ul>
-            {menuItems.map((item, index) => (
-              <li
-                key={index}
-                onClick={() => {
-                  setIsActive && setIsActive(!isActive);
-                }}
-              >
+    <div className={`container-nav ${className}  ${isActive ? "active" : ""}`}>
+      <nav>
+        <ul>
+          {menuItems.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => {
+                setIsActive && setIsActive(!isActive);
+              }}
+            >
+              {item.icon && (
                 <div className="container-icono">
                   <Icono icono={item.icon} />
                 </div>
-                <A href={item.href}>{item.label}</A>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </TouchNav>
+              )}
+              <A href={item.href}>{item.label}</A>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 };
 

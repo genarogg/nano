258f877e8a6e8c69c761d225bb36fa2d69da0156
Genaro Gonzalez <genarrogg@gmail.com>
2024-11-17 11:08:50 -0800

    links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ const index: React.FC<indexProps> = () => {
   return (
     <div className="home">
       <A href="/headers">headers</A>
+      <A href="/buttons">buttons</A>
+      <A href="/inputs">inputs</A>
     </div>
   );
 };
